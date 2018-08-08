20609c5b8f570e02a560b18da9c9afcc54dbef65
25steps <a_marushin@mail.ru>
2018-08-08 15:09:06 +0300

    Создан проект Кошки

---
+++ src/Cats/Catsclass.java
@@ -0,0 +1,14 @@
+package Cats;
+
+public class Catsclass {
+    private String name;
+    private int age;
+    private String colour;
+
+    public Catsclass(String name, int age, String colour) {
+        this.name = name;
+        this.age = age;
+        this.colour = colour;
+    }
+
+}
---
+++ src/Cats/Catsmain.java
@@ -0,0 +1,12 @@
+package Cats;
+
+public class Catsmain {
+    public static void main(String[] args) {
+        Catsclass cats[] = new Catsclass[5];
+        cats[0] = new Catsclass("Barsik", 5, "Black");
+        cats[1] = new Catsclass("Pussy", 1, "White");
+        cats[2] = new Catsclass("Tolstya", 10, "Red");
+        cats[3] = new Catsclass("Redsy", 2, "Black and White");
+        cats[4] = new Catsclass("Stormy", 3, "Grey");
+    }
+}
