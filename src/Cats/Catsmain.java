3b58879dd1f2d1b950f106c4460958edfaa6df95
25steps <a_marushin@mail.ru>
2018-08-28 11:40:02 +0300

    Изменение инфы

--- src/Cats/Catsmain.java
+++ src/Cats/Catsmain.java
@@ -3,11 +3,11 @@ package Cats;
 public class Catsmain {
     public static void main(String[] args) {
         Catsclass cats[] = new Catsclass[5];
-        cats[0] = new Catsclass("Barsik", 5, "Black");
-        cats[1] = new Catsclass("Pussy", 1, "White");
-        cats[2] = new Catsclass("Tolstya", 10, "Red");
-        cats[3] = new Catsclass("Redsy", 2, "Black and White");
-        cats[4] = new Catsclass("Stormy", 3, "Grey");
+        cats[0] = new Catsclass("Barsik", 3, "Black");
+        cats[1] = new Catsclass("Pussy", 2, "White");
+        cats[2] = new Catsclass("Tolstya", 11, "Red");
+        cats[3] = new Catsclass("Redsy", 5, "Black and White");
+        cats[4] = new Catsclass("Stormy", 6, "Grey");
 
         for (int i = 0; i < cats.length; i++) {
             System.out.println("Name: "+cats[i].getName());
