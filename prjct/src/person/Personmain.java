0fa775fe142a4cc3e217b957b2849a1aac2ed66b
25steps <a_marushin@mail.ru>
2018-08-28 19:37:42 +0300

    модификация нового проекта

--- prjct/src/person/Personmain.java
+++ prjct/src/person/Personmain.java
@@ -3,6 +3,22 @@ package person;
 
 public class Personmain {
     public static void main(String[] args) {
-        System.out.println("какой-то код");
+       int sum = 0;
+       double avg = 0.0;
+       Persons[] persons = new Persons[5];
+       persons[0] = new Persons("Drew",26);
+       persons[1] = new Persons("Alex",18);
+       persons[2] = new Persons("Nik",23);
+       persons[3] = new Persons("Diman",36);
+       persons[4] = new Persons("Evgeniy",19);
+
+        System.out.println("Участники отряда Драккарня");
+        for (int i = 0; i < persons.length; i++) {
+            System.out.println(i+1 +": " + persons[i].getName());
+            sum += persons[i].getAge();
+        }
+        avg = sum/persons.length;
+        System.out.println("Средний возраст отряда: "+ avg);
+
     }
 }
---
+++ prjct/src/person/Persons.java
@@ -0,0 +1,24 @@
+package person;
+
+public class Persons {
+    private String name;
+    private int age;
+    private  int sum;
+    private  double avg;
+
+
+
+    public Persons(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
